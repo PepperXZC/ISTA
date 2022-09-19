ade4a3f642793c357125904802aff74322505ed8
PepperXZC <pepiipepper@gmail.com>
2022-09-19 14:05:42 +0800

    original files

---
+++ 交替迭代/ISTA/ISTAnet.py
@@ -0,0 +1,288 @@
+from tkinter.tix import Tree
+from turtle import forward
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.normal import Normal
+from .modelio import LoadableModel, store_config_args
+import tensorflow as tf
+from . import layers
+device = 'cuda'
+# def iteration(k:int, x:torch.tensor, )
+
+class G_block(nn.Module):
+    def __init__(self, inshape=None):
+        super(G_block, self).__init__()
+
+        ndims = len(inshape)
+        assert ndims in [1, 2, 3], 'ndims should be one of 1, 2, or 3. found: %d' % ndims
+
+        # self.network = nn.ModuleList()
+        # for i in range(iter_num):
+        self.conv = nn.Sequential(
+            nn.Conv3d(3,16,3,1,1), # padding = 1
+            nn.Conv3d(16,32,3,1,1),
+            nn.ReLU(),
+            nn.Conv3d(32,32,3,1,1)
+        )
+    
+    def forward(self,x):
+        return self.conv(x)
+
+class G_back_block(nn.Module):
+    def __init__(self, inshape=None):
+        super(G_back_block, self).__init__()
+
+        ndims = len(inshape)
+        assert ndims in [1, 2, 3], 'ndims should be one of 1, 2, or 3. found: %d' % ndims
+
+        # self.network = nn.ModuleList()
+        # for i in range(iter_num):
+        self.conv = nn.Sequential(
+            nn.Conv3d(32,32,3,1,1),
+            nn.ReLU(),
+            nn.Conv3d(32,16,3,1,1),
+            nn.Conv3d(16,3,3,1,1), # padding = 1
+        )
+    
+    def forward(self,x):
+        return self.conv(x)
+
+class softhreshold(nn.Module): # soft函数
+    def __init__(self,inshape):
+        super(softhreshold,self).__init__()
+        self.theta = nn.Parameter(torch.randn(inshape), requires_grad=True).to(device)
+    
+    def forward(self, x:torch.tensor):
+        return torch.sign(x) * torch.max(torch.abs(x) - self.theta, torch.zeros(x.shape).to(device))
+
+class NLBlockND(nn.Module):
+    def __init__(self, in_channels, inter_channels=None, mode='embedded', 
+                 dimension=3, bn_layer=True):
+        """Implementation of Non-Local Block with 4 different pairwise functions but doesn't include subsampling trick
+        args:
+            in_channels: original channel size (1024 in the paper)
+            inter_channels: channel size inside the block if not specifed reduced to half (512 in the paper)
+            mode: supports Gaussian, Embedded Gaussian, Dot Product, and Concatenation
+            dimension: can be 1 (temporal), 2 (spatial), 3 (spatiotemporal)
+            bn_layer: whether to add batch norm
+        """
+        super(NLBlockND, self).__init__()
+
+        assert dimension in [1, 2, 3]
+        
+        if mode not in ['gaussian', 'embedded', 'dot', 'concatenate']:
+            raise ValueError('`mode` must be one of `gaussian`, `embedded`, `dot` or `concatenate`')
+            
+        self.mode = mode
+        self.dimension = dimension
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+
+        # the channel size is reduced to half inside the block
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+        
+        # assign appropriate convolutional, max pool, and batch norm layers for different dimensions
+        if dimension == 3:
+            conv_nd = nn.Conv3d
+            max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
+            bn = nn.BatchNorm3d
+        elif dimension == 2:
+            conv_nd = nn.Conv2d
+            max_pool_layer = nn.MaxPool2d(kernel_size=(2, 2))
+            bn = nn.BatchNorm2d
+        else:
+            conv_nd = nn.Conv1d
+            max_pool_layer = nn.MaxPool1d(kernel_size=(2))
+            bn = nn.BatchNorm1d
+
+        # function g in the paper which goes through conv. with kernel size 1
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+
+        # add BatchNorm layer after the last conv layer
+        if bn_layer:
+            self.W_z = nn.Sequential(
+                    conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels, kernel_size=1),
+                    bn(self.in_channels)
+                )
+            # from section 4.1 of the paper, initializing params of BN ensures that the initial state of non-local block is identity mapping
+            nn.init.constant_(self.W_z[1].weight, 0)
+            nn.init.constant_(self.W_z[1].bias, 0)
+        else:
+            self.W_z = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels, kernel_size=1)
+
+            # from section 3.3 of the paper by initializing Wz to 0, this block can be inserted to any existing architecture
+            nn.init.constant_(self.W_z.weight, 0)
+            nn.init.constant_(self.W_z.bias, 0)
+
+        # define theta and phi for all operations except gaussian
+        if self.mode == "embedded" or self.mode == "dot" or self.mode == "concatenate":
+            self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+            self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1)
+        
+        if self.mode == "concatenate":
+            self.W_f = nn.Sequential(
+                    nn.Conv2d(in_channels=self.inter_channels * 2, out_channels=1, kernel_size=1),
+                    nn.ReLU()
+                )
+            
+    def forward(self, x):
+        """
+        args
+            x: (N, C, T, H, W) for dimension=3; (N, C, H, W) for dimension 2; (N, C, T) for dimension 1
+        """
+
+        batch_size = x.size(0)
+        
+        # (N, C, THW)
+        # this reshaping and permutation is from the spacetime_nonlocal function in the original Caffe2 implementation
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        if self.mode == "gaussian":
+            theta_x = x.view(batch_size, self.in_channels, -1)
+            phi_x = x.view(batch_size, self.in_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            f = torch.matmul(theta_x, phi_x)
+
+        elif self.mode == "embedded" or self.mode == "dot":
+            theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+            phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+            theta_x = theta_x.permute(0, 2, 1)
+            f = torch.matmul(theta_x, phi_x)
+
+        elif self.mode == "concatenate":
+            theta_x = self.theta(x).view(batch_size, self.inter_channels, -1, 1)
+            phi_x = self.phi(x).view(batch_size, self.inter_channels, 1, -1)
+            
+            h = theta_x.size(2)
+            w = phi_x.size(3)
+            theta_x = theta_x.repeat(1, 1, 1, w)
+            phi_x = phi_x.repeat(1, 1, h, 1)
+            
+            concat = torch.cat([theta_x, phi_x], dim=1)
+            f = self.W_f(concat)
+            f = f.view(f.size(0), f.size(2), f.size(3))
+        
+        if self.mode == "gaussian" or self.mode == "embedded":
+            f_div_C = F.softmax(f, dim=-1)
+        elif self.mode == "dot" or self.mode == "concatenate":
+            N = f.size(-1) # number of position in x
+            f_div_C = f / N
+        
+        y = torch.matmul(f_div_C, g_x)
+        
+        # contiguous here just allocates contiguous chunk of memory
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
+        
+        W_y = self.W_z(y)
+        # residual connection
+        z = W_y + x
+
+        return z
+
+# class NLBlockND(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+
+class EPN_Net(LoadableModel):
+    @store_config_args
+    def __init__(self, inshape, f_function=None, iteration_S=int):
+        super().__init__()
+        # self.alpha = torch.randn(inshape).to(device)# 或许需要初始化函数的
+        self.alpha = []
+        
+        self.gamma = []
+        self.theta = []
+        self.history = []
+        # self.iter_num = iter_num * 2
+        self.f = f_function
+        self.transformer = layers.SpatialTransformer(inshape)
+        self.block_sequence = nn.ModuleList()
+
+        self.forward_block = nn.Sequential(
+            G_block(inshape=inshape),
+            softhreshold(inshape=inshape),
+            NLBlockND(in_channels=32),
+            G_back_block(inshape=inshape)
+        )
+
+        self.back_block = nn.Sequential(
+            G_block(inshape=inshape),
+            softhreshold(inshape=inshape),
+            NLBlockND(in_channels=32),
+            G_back_block(inshape=inshape)
+        )
+
+        for i in range(iteration_S):
+            self.block_sequence.append(nn.Sequential(self.forward_block, self.back_block))
+
+            self.alpha.append(nn.Parameter(torch.randn(inshape), requires_grad=True).to(device))
+            self.gamma.append(nn.Parameter(torch.randn(inshape), requires_grad=True).to(device))
+            self.theta.append(nn.Parameter(torch.randn(inshape), requires_grad=True).to(device))
+
+        self.iteration = iteration_S
+        self.beta = self.alpha
+        # for i in range(self.)
+    
+    def forward(self,x, f, m): # k: x为图片列表中第k项
+        '''
+        x: phi
+        m: moving img
+        f: fixed img
+        '''
+        # x_history: [x_+0.5]
+        # 注：需要修改generator格式，使其每次执行时告诉模型，对应的图像是哪张
+
+        for k in range(self.iteration):
+            if len(self.history) != 0:
+                x_tieta_k = x + self.gamma[k] * (x  - self.history[-1]) # 上一个，就是上一phase中的第二个
+            else:
+                x_tieta_k = x
+            
+            # self.f.zero_grad()
+            loss = self.f(f,self.transformer(m, x))# 标量梯度？
+            x.retain_grad()
+            loss.backward()
+            # y = self.f(x).backward()   
+            b_half = x_tieta_k - self.alpha[k] * x.grad #requires_grad = true
+            # x_forward = self.network1(b_half)
+            # x_forward = self.soft_function(x_forward,self.theta)
+            # x_forward = self.NLBlock(x_forward)
+            # x_forward = self.network2(x_forward)
+            x_forward = self.block_sequence[k][0](b_half)
+            x_half = x_forward + b_half
+
+            self.history = [x_half]
+
+            # self.f.zero_grad()
+            x_final = x_half + self.gamma[k] * (x_half - x)
+            y = self.f(f, self.transformer(m, x_final))
+            x_final.retain_grad()
+            y.backward()
+            b = x_final - self.beta[k] * x_final.grad
+
+            # x_backward = self.network1(b)
+            # x_backward = self.soft_function(x_backward,self.theta)
+            # x_backward = self.NLBlock(x_backward)
+            # x_backward = self.network2(x_backward)
+            b = self.block_sequence[k][0](b)
+            x += b # x_k+1
+        return x
+
+
+
+            
+
+p = torch.tensor([1,2,3]).float().requires_grad_(True)
+q = torch.tensor([7,8,9]).float().requires_grad_(True)
+y = p * q
+y.sum().backward()
+print(p.grad)
+
---
+++ 交替迭代/train.py
@@ -0,0 +1,249 @@
+from statistics import mode
+from ISTA import ISTAnet
+from vxm import networks, layers
+import torch
+import generator
+import os
+import random
+import argparse
+import time
+import numpy as np
+import torch
+from tqdm import tqdm
+import voxelmorph as vxm  # nopep8
+# from torch.utils.tensorboard import SummaryWriter
+
+# import voxelmorph with pytorch backend
+os.environ['VXM_BACKEND'] = 'pytorch'
+
+class MSE:
+    """
+    Sigma-weighted mean squared error for image reconstruction.
+    """
+
+    def __init__(self, image_sigma=1.0):
+        self.image_sigma = image_sigma
+
+    def mse(self, y_true, y_pred):
+        return torch.square(y_true - y_pred)
+
+    def loss(self, y_true, y_pred, reduce='mean'):
+        # compute mse
+        mse = self.mse(y_true, y_pred)
+        # reduce
+        if reduce == 'mean':
+            mse = torch.mean(mse)
+        elif reduce == 'max':
+            mse = torch.max(mse)
+        elif reduce is not None:
+            raise ValueError(f'Unknown MSE reduction type: {reduce}')
+        # loss
+        return 1.0 / (self.image_sigma ** 2) * mse
+
+# parse the commandline
+parser = argparse.ArgumentParser()
+
+# data organization parameters
+parser.add_argument('--img-list', help='line-seperated list of training files',
+    default=r"D:\Study\projects\Register\dataset\file_list.txt")
+parser.add_argument('--img-prefix', help='optional input image file prefix')
+parser.add_argument('--img-suffix', help='optional input image file suffix')
+parser.add_argument('--atlas', help='atlas filename (default: data/atlas_norm.npz)')
+parser.add_argument('--model-dir', default='models',
+                    help='model output directory (default: models)')
+parser.add_argument('--multichannel', action='store_true',
+                    help='specify that data has multiple channels')
+
+# training parameters
+parser.add_argument('--gpu', default='0', help='GPU ID number(s), comma-separated (default: 0)')
+parser.add_argument('--batch-size', type=int, default=1, help='batch size (default: 1)')
+parser.add_argument('--epochs', type=int, default=1500,
+                    help='number of training epochs (default: 1500)')
+parser.add_argument('--steps-per-epoch', type=int, default=100,
+                    help='frequency of model saves (default: 100)')
+parser.add_argument('--load-model1', help='optional model file to initialize with')
+parser.add_argument('--load-model2', help='optional model file to initialize with')
+parser.add_argument('--initial-epoch', type=int, default=0,
+                    help='initial epoch number (default: 0)')
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 1e-4)')
+parser.add_argument('--cudnn-nondet', action='store_true',
+                    help='disable cudnn determinism - might slow down training')
+
+# network architecture parameters
+parser.add_argument('--enc', type=int, nargs='+',
+                    help='list of unet encoder filters (default: 16 32 32 32)')
+parser.add_argument('--dec', type=int, nargs='+',
+                    help='list of unet decorder filters (default: 32 32 32 32 32 16 16)')
+parser.add_argument('--int-steps', type=int, default=7,
+                    help='number of integration steps (default: 7)')
+parser.add_argument('--int-downsize', type=int, default=2,
+                    help='flow downsample factor for integration (default: 2)')
+parser.add_argument('--bidir', action='store_true', help='enable bidirectional cost function')
+
+# loss hyperparameters
+parser.add_argument('--image-loss', default='mse',
+                    help='image reconstruction loss - can be mse or ncc (default: mse)')
+parser.add_argument('--lambda', type=float, dest='weight', default=0.01,
+                    help='weight of deformation loss (default: 0.01)')
+args = parser.parse_args()
+
+bidir = args.bidir
+
+train_files = vxm.py.utils.read_file_list(args.img_list, prefix=args.img_prefix,
+                                          suffix=args.img_suffix)
+assert len(train_files) > 0, 'Could not find any training data.'
+
+# no need to append an extra feature axis if data is multichannel
+add_feat_axis = not args.multichannel
+
+def distance(self, vector1,vector2):
+    return torch.sqrt(sum(pow(vector2-vector1,2)))  # pow()是自带函数
+
+if args.atlas:
+    # scan-to-atlas generator
+    atlas = vxm.py.utils.load_volfile(args.atlas, np_var='vol',
+                                      add_batch_axis=True, add_feat_axis=add_feat_axis)
+    dataset = generator.scan_to_atlas(train_files, atlas,
+                                             batch_size=args.batch_size, bidir=args.bidir,
+                                             add_feat_axis=add_feat_axis)
+else:
+    # scan-to-scan generator
+    dataset = generator.scan_to_scan(
+        train_files, batch_size=args.batch_size, bidir=args.bidir, add_feat_axis=add_feat_axis)
+
+# extract shape from sampled input
+[index_1, index_2], inshape = next(dataset)
+in_shape = inshape[0][0].shape[1:-1]
+print(in_shape)
+
+# 模型文件存放
+print(torch.randn(3))
+model_dir = args.model_dir
+os.makedirs(model_dir, exist_ok=True)
+
+# 获取gpu信息
+gpus = args.gpu.split(',')
+nb_gpus = len(gpus)
+device = 'cuda'
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+torch.backends.cudnn.deterministic = not args.cudnn_nondet
+
+enc_nf = args.enc if args.enc else [16, 32, 32, 32]
+dec_nf = args.dec if args.dec else [32, 32, 32, 32, 32, 16, 16]
+
+vxm_loss_func = MSE().loss # 或NCC
+weights = [1, 0.01] #后一项是lambda
+
+# x_history = {}
+# for i in range(30): # 目前假定数据集共30张图片
+#     x_history[i] = 0
+
+if args.load_model1 and args.load_model2:
+    # load initial model (if specified)
+    model = networks.VxmDense.load(args.load_model1, device)
+    mini_model = ISTAnet.EPN_Net.load(args.load_model2, device)
+else:
+    # otherwise configure new model
+    model = networks.VxmDense(
+        inshape=in_shape,
+        nb_unet_features=[enc_nf, dec_nf],
+        bidir=bidir,
+        int_steps=args.int_steps,
+        int_downsize=args.int_downsize
+    )
+    mini_model = ISTAnet.EPN_Net(inshape=in_shape,f_function=vxm_loss_func, iteration_S=30)
+
+if nb_gpus > 1:
+    # use multiple GPUs via DataParallel
+    model = torch.nn.DataParallel(model)
+    model.save = model.module.save
+    mini_model = torch.nn.DataParallel(mini_model)
+    mini_model.save = mini_model.module.save
+model = model.to(device)
+mini_model = mini_model.to(device)
+
+optimizer_vxm = torch.optim.Adam(model.parameters(), lr=args.lr)
+optimizer_epn = torch.optim.Adam(mini_model.parameters(), lr=args.lr)
+transformer = layers.SpatialTransformer(in_shape)
+
+# writer = SummaryWriter()
+# input_f = torch.rand(in_shape)
+# input_m = torch.rand(in_shape)
+# writer.add_graph(mini_model, x_history, input_f, input_m)
+
+# writer.close()
+
+
+
+print("start")
+for epoch in range(args.initial_epoch, args.epochs):
+
+    # save model checkpoint
+    if epoch % 20 == 0:
+        model.save(os.path.join(model_dir, '%04d.pt' % epoch))
+
+    epoch_loss = []
+    epoch_total_loss = []
+    epoch_step_time = []
+
+    # for step in tqdm(range(args.steps_per_epoch)):
+    for step in range(args.steps_per_epoch):
+
+        step_start_time = time.time()
+
+        # generate inputs (and true outputs) and convert them to tensors
+        [index_1, index_2], (inputs, y_true) = next(dataset) # 以m图为主
+        # x_history[index_1]
+        inputs = [torch.from_numpy(d).to(device).float().permute(0, 4, 1, 2, 3) for d in inputs] # return [1, 1, 144, 192, 160] * 2, m, f
+        y_true = [torch.from_numpy(d).to(device).float().permute(0, 4, 1, 2, 3) for d in y_true] # return [1, 1, 144, 192, 160] * 2, f, 0
+        # [1, 1, 144, 192, 160]; [1, 3, 144, 192, 160]
+        # y_true中后一组数为0
+        for data in inputs:
+            data.requires_grad_(True)
+        # run inputs through the model to produce a warped image and flow field
+        optimizer_vxm.zero_grad()
+        y_pred = model(*inputs)  # return [1, 1, 144, 192, 160] * 2: m + phi, phi
+        # m + phi: [1, 1, 144, 192, 160]; phi: [1, 3, 144, 192, 160]
+        first_loss = vxm_loss_func(y_true[0],y_pred[0])
+        for data in y_pred:
+            data.requires_grad_(True)
+        
+        # 应该是
+        # phi的思路不改，增加训练epoch数大小
+        optimizer_epn.zero_grad()
+        phi_pred = mini_model(y_pred[1],y_true[0],inputs[0])
+        x_pred = transformer(inputs[1],phi_pred)
+        second_loss = distance(x_pred, y_true[0])
+
+        loss_list = [first_loss, second_loss]
+
+        first_loss.backward()
+        optimizer_vxm.step()
+        second_loss.backward()
+        optimizer_epn.step()
+        
+
+        # calculate total loss
+        # loss = 0
+        # loss_list = []
+        # for n, loss_function in enumerate(losses): #1: f, m+phi; 2: 0, phi
+        #     curr_loss = loss_function(y_true[n], y_pred[n]) * weights[n]
+        #     loss_list.append(curr_loss.item())
+        #     loss += curr_loss
+
+        
+
+        # get compute time
+        epoch_step_time.append(time.time() - step_start_time)
+
+    # print epoch info
+    epoch_info = 'Epoch %d/%d' % (epoch + 1, args.epochs)
+    time_info = '%.4f sec/step' % np.mean(epoch_step_time)
+    losses_info = ', '.join(['%.4e' % f for f in loss_list])
+    loss_info = 'first loss:{num1}, second loss:{num2}'.format(num1=first_loss, num2=second_loss)
+    print(' - '.join((epoch_info, time_info, loss_info)), flush=True)
+
+# final model save
+model.save(os.path.join(model_dir, '%04d.pt' % args.epochs))
+
